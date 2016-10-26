c710e98488cf9c0d5ee81d0155a4b8d682c78448
Andrew Hessler <andrewhessloch@gmail.com>
2016-10-26 18:47:41 -0500

    Download from search available

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
 $name = $_GET['q'];
-
+$name = '%' . $name . '%';
 $host = '127.0.0.1';
 $db = 'studentarchive';
 $user = 'andrew';
@@ -18,7 +18,40 @@ $pdo = new PDO($dsn, $user, $pass, $opt);
 
 $sql = "SELECT * FROM Document WHERE class IN (SELECT id FROM Class WHERE name LIKE :name)";
 $stmt = $pdo->prepare($sql);
-$stmt->execute(['name' => $q]);
+$stmt->execute(['name' => $name]);
 
-echo "Hello";
+while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+
+	$sql = "SELECT name FROM Class WHERE id = :id";
+	$varstmt = $pdo->prepare($sql);
+	$varstmt->execute(['id'=>$row['class']]);
+	$class = $varstmt->fetch(PDO::FETCH_ASSOC)['name'];
+	
+	$sql = "SELECT name FROM Teacher WHERE id = :id";
+	$varstmt = $pdo->prepare($sql);
+	$varstmt->execute(['id'=>$row['teacher']]);
+	$teacher = $varstmt->fetch(PDO::FETCH_ASSOC)['name'];
+
+	$sql = "SELECT name FROM Semester WHERE id = :id";
+	$varstmt = $pdo->prepare($sql);
+	$varstmt->execute(['id'=>$row['semester']]);
+	$semester = $varstmt->fetch(PDO::FETCH_ASSOC)['name'];
+
+	$sql = "SELECT name FROM DocType WHERE id = :id";
+	$varstmt = $pdo->prepare($sql);
+	$varstmt->execute(['id'=>$row['doctype']]);
+	$doctype = $varstmt->fetch(PDO::FETCH_ASSOC)['name'];
+
+	$file = $doctype . "_" . $row['num'] . "-" . $row['iteration'];
+	echo "\\" . $class . "<br/>";
+	echo str_repeat('&nbsp;',2) . "\\" . $teacher . "<br/>";
+	echo str_repeat('&nbsp;',4) . "\\" . $semester . "<br/>";
+	echo str_repeat('&nbsp;', 6) .
+	"<a href=\"files/$class/$teacher/$semester/$file\" download>" . 
+	$file . "<br/>";
+
+}
+$pdo = null;
+
 ?>
