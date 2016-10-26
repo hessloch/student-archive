cfc22d8d2bac3c25d8961ae4b11a5a07d020f885
Andrew Hessler <andrewhessloch@gmail.com>
2016-10-26 17:40:24 -0500

    live search working, hash out details

--- search.php
+++ search.php
@@ -1,22 +1,24 @@
 <?php
 $name = $_GET['q'];
-$name = '%' + $name '%';
 
 $host = '127.0.0.1';
-$db = 'test';
-$user = 'root';
+$db = 'studentarchive';
+$user = 'andrew';
 $pass = 'pass487project';
 $charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $opt = [
-	PDO:ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	PDO::ATR_EMULATE_PREPARES => false,
+	PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $pdo = new PDO($dsn, $user, $pass, $opt);
 
 $sql = "SELECT * FROM Document WHERE class IN (SELECT id FROM Class WHERE name LIKE :name)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute(['name' => $q]);
+
+echo "Hello";
+?>
